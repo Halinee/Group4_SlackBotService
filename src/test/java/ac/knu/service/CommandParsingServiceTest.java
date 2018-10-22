5813dbdbc0b00da0d8055c8301d9b38f64d8e9cb
wengers1004 <wengers1004@naver.com>
2018-10-22 22:49:36 +0900

    command Parsing Service Test

--- src/test/java/ac/knu/service/CommandParsingServiceTest.java
+++ src/test/java/ac/knu/service/CommandParsingServiceTest.java
@@ -1,12 +1,51 @@
 package ac.knu.service;
 
+import ac.knu.service.exception.UnprocessableCommandException;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class CommandParsingServiceTest {
 
+    private CommandParsingService commandParsingService;
+    private HashMap<String, Friend> testDatabase;
+
+    @Before
+    public void setUp() {
+        commandParsingService = new CommandParsingService();
+        testDatabase = new HashMap<String, Friend>();
+    }
     @Test
-    public void bot_should_understand_list_command() {
-        CommandParsingService commandParsingService = new CommandParsingService();
-        commandParsingService.parseCommand("list");
+    public void bot_should_understand_Add_command() throws UnprocessableCommandException {
+        String result = commandParsingService.parseCommand(testDatabase, "Add");
+        assertTrue(result.contains("추가되었습니다."));
+    }
+
+    @Test
+    public void bot_should_understand_Remove_command() throws UnprocessableCommandException {
+        String result = commandParsingService.parseCommand(testDatabase, "Remove");
+        assertTrue(result.contains("삭제되었습니다."));
+    }
+
+    @Test
+    public void bot_should_understand_List_command() throws UnprocessableCommandException {
+        String result = commandParsingService.parseCommand(testDatabase, "List");
+        assertTrue(result.contains("친구 목록"));
+    }
+
+    @Test
+    public void bot_should_understand_Find_command() throws UnprocessableCommandException {
+        String result = commandParsingService.parseCommand(testDatabase, "Find");
+        assertTrue(result.contains("찾았습니다."));
+    }
+
+    @Test
+    public void bot_should_understand_Time_command() throws UnprocessableCommandException {
+        String result = commandParsingService.parseCommand(testDatabase, "Time");
+        assertTrue(result.contains("현재 시간 : "));
     }
 }
